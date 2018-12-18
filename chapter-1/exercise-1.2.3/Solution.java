b5de195ccabc56beb66c535bd5d0fa72605b3608
Ka Wong <kawong2020@u.northwestern.edu>
2018-12-17 19:14:12 -0600

    q2 and q3

--- chapter-1/exercise-1.2.3/Solution.java
+++ chapter-1/exercise-1.2.3/Solution.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+import java.util.Locale;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
 
 public class Solution{
 
@@ -15,8 +19,39 @@ public class Solution{
         System.out.format("%7.3f%n", inData);
     }
 
+
+    /**
+     * @param n
+     * print pi to n digits after the decimal point (rounded).
+     */
+    public static void EchoPiDigits(int n) {
+        String form = "%." + n + "f%n";
+        System.out.format(form, Math.PI);
+    }
+
+    /**
+     * @param date
+     * given a date, determine the day of the week
+     */
+    public static void getDay(String date) {
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat format = new SimpleDateFormat("dd MMMM yyyy", Locale.ENGLISH);
+        try {
+            c.setTime(format.parse(date));
+            System.out.println(c.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.US));
+        } catch (ParseException e) {
+            System.out.println("Invalid date");
+        }
+    }
+
+    public static void sortDistinctInts(int[] arr) {
+        
+    }
+
     public static void main(String arg[]){
-        System.out.println("Hello world");
-        readDouble();
+//        readDouble();
+//        EchoPiDigits(4);
+//        getDay("9 August 2010");
+
     }
 }
